cfef19802bcb6d58178afb7ef7a7b5b0db661bd1
thomasridgeon <222263380+thomasridgeon@users.noreply.github.com>
2025-07-28 16:28:11 -0400

    update to code

--- itryruby.rb
+++ itryruby.rb
@@ -23,3 +23,29 @@ puts "you entered #{number}"
 var1 = number
 puts 'let\'s add 666 to your favourite number'
 puts var1 + 666.to_i
+#
+puts "how can I help you?"
+user_input = gets.chomp
+puts "WHAT DO YOU MEAN #{user_input.upcase}?!?!?!"
+# 
+puts "you should write a book"
+puts ""
+lineWidth = 50
+puts "Table of Contents".center(lineWidth)
+chapter1_title = "Chatper 1: #{user_input}"
+chapter1_page = "Page 1"
+#calculate the space needed between title and page below:
+space_needed = lineWidth - chapter1_title.length - chapter1_page.length
+puts chapter1_title + (" " * space_needed) + chapter1_page
+#
+puts "do you want me to give you a random number between 1-100? yes or no?"
+user_response = gets.chomp.downcase
+if user_response == "yes"
+    random_number = (rand (100))
+puts "okay, you're random number is #{random_number}"
+elsif user_response == "no"
+puts "okay, no problem"
+else puts "I din't understand. Please answer 'yes' or 'no'"
+end
+
+
